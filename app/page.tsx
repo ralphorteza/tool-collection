d85f10ee57ad2d2fcd3e5db08c12539716a32c85
ralph orteza <ralphlorteza@gmail.com>
2023-10-16 09:05:45 -0700

    Migrate table to app/

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,11 @@
 import GetServerData from "@/lib/get-server-data"
-
+import { columns } from "./columns";
+import { DataTable } from "./data-table";
 export default async function Home() {
   const tools = await GetServerData();
   return (
-    <div>
-      hello world
+    <div className="container mx-auto py-10">
+      <DataTable columns={columns} data={tools} />
     </div>
   )
 }
